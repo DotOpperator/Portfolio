771829df03d9b484e201985d7fac8790be22a5cd
Manohar Chitoda <manohar.chitoda@rutgers.edu>
2020-06-04 11:44:31 -0400

    updated the snake

--- Snake/JS & HTML5/draw.js
+++ Snake/JS & HTML5/draw.js
@@ -9,4 +9,17 @@ var snake;
 (function setup () {
 	snake = new Snake();
 	snake.draw();
+
+	window.setInterval(() =>{
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		snake.update();
+		snake.draw();
+	},250);
 }());
+
+window.addEventListener('keydown', ((evt) => {
+	console.log(evt);
+	const direction = evt.key.replace('Arrow','');
+	snake.changeDirection(direction);
+	
+}))
