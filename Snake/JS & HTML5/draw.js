7e624589631c5291659f46cb9ad9b8de2fb40c94
Manohar Chitoda <manohar.chitoda@rutgers.edu>
2020-06-05 16:28:22 -0400

    Finished the basic snake game!

--- Snake/JS & HTML5/draw.js
+++ Snake/JS & HTML5/draw.js
@@ -1,21 +1,31 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 const scale = 10;
-const row = canvas.height / scale;
+const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 
 var snake;
 
+// setting up the canvas
 (function setup () {
 	snake = new Snake();
 	fruit = new Fruit();
-	fruit.pickLocation();
+
+	fruit.pickLocation(); // pick the location of the fruit
 
 	window.setInterval(() =>{
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		fruit.draw();
 		snake.update();
 		snake.draw();
-		fruit.draw();
+
+		// Pick new location for the fruit
+		// if the snake ate the fruit
+		if (snake.eat(fruit)) {
+			fruit.pickLocation();
+		}
+		snake.checkCollision();
+		document.querySelector('.score').innerText = snake.total;
 	},250);
 }());
 
--- Snake/JS & HTML5/snake.js
+++ Snake/JS & HTML5/snake.js
@@ -3,13 +3,29 @@ function Snake() {
 	this.y = 0;
 	this.xSpeed = scale * 1;
 	this.ySpeed = 0;
+	this.total = 0;
+	this.tail = [];
 
+	// Draw the snake
 	this.draw = function () {
-		ctx.fillStyle = "#FFFFFF";
+		// color of the snake
+		ctx.fillStyle = "#FFFFFF";	
+		
+		// Draw the length of the snake based on the length of it's tail
+		for(let i = 0; i < this.tail.length;i++){
+			ctx.fillRect(this.tail[i].x,this.tail[i].y,scale,scale);
+		}
+		
 		ctx.fillRect(this.x,this.y,scale,scale);
 	}
 
 	this.update = function(){
+		for (let i = 0; i < this.tail.length -1; i++){
+			this.tail[i] = this.tail[i+1];
+		}
+
+		this.tail[this.total - 1] = {x: this.x, y: this.y };
+
 		this.x += this.xSpeed;
 		this.y += this.ySpeed;
 		
@@ -48,6 +64,23 @@ function Snake() {
 				this.xSpeed = scale * 1;
 				this.ySpeed = 0;
 				break;
+		}
+	}
+
+	this.eat = function(fruit){
+		if(this.x == fruit.x && this.y == fruit.y){
+			this.total++;
+			return true;
+		}
+		return false;
+	}
+
+	this.checkCollision = function(){
+		for (var i = 0; i < this.tail.length; i++) {
+			if(this.x == this.tail[i].x && this.y == this.tail[i].y){
+				this.total = 0;
+				this.tail[] = [];
+			}
 		}
 	}
 }
